0337886203067060178c0968566b77b74fd9aa94
JecoLantern <jbayonke@hotmail.com>
2018-11-16 20:53:39 -0800

    bug fix

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlendoded({extended: true}));
-app.use(express.text());
-app.use(expree.json({type: "application/vnd.api+json"}));
+app.use(express.urlencoded({extended: true}));
+// app.use(express.text());
+app.use(express.json({type: "application/vnd.api+json"}));
 app.use(express.static(__dirname + "/app/public"));
 
 require("./app/routing/apiRoutes")(app);
